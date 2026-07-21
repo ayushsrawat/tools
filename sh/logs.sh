63cb7140a8782686a307184326fec816596fa225
Ayush Rawat <ayushrawat3338@gmail.com>
2026-07-22 00:37:25 +0530

    Introduce logs.sh to tail the log file and use awk to color lines

---
+++ sh/logs.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+CYAN=$(tput setaf 39)
+RESET=$(tput sgr0)
+
+function usage() {
+  printf "${GREEN} Usage:\n\t./logs.sh <file-to-tail>\n${RESET}"
+}
+
+FILE_PATH="$1"
+
+if [[ ! -f "$FILE_PATH" ]]; then
+  printf "${CYAN} Invalid file: ${FILE_PATH} \n${RESET}"
+  usage
+  exit 1
+fi
+
+printf "${CYAN}Logging...\n"
+printf "running command : tail -f ${FILE_PATH}${RESET}\n"
+
+# tail the log file and use awk to color lines
+tail -f $FILE_PATH | awk -v red="$RED" -v yellow="$YELLOW" -v cyan="$CYAN" -v reset="$RESET" '
+{
+  if ($0 ~ /ERROR|Exception|SEVERE|FATAL|FAIL|^\s+at / || $0 ~ /^java\./) {
+    print red $0 reset
+  } else if ($0 ~ /WARNING|WARN/) {
+    print yellow $0 reset
+  } else {
+     print cyan $0 reset
+  }
+  fflush()
+}'
